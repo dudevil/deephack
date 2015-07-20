bdb25d151b6cace67ca9ae08ce083e6d447be52d
Marat Zaynutdinov <tsundokum@gmail.com>
2015-07-20 22:01:20 +0300

    frugal method

---
+++ dqn/helpers.lua
@@ -0,0 +1,17 @@
+
+math = require "math"
+
+function frugal(init_estimate, percentile)
+    local percentile = percentile or 0.5
+    local estimate = init_estimate
+
+    return function(element)
+        local r = math.random()
+        if element > estimate and r > 1-percentile then
+            estimate = estimate + 1
+        elseif element < estimate and r > percentile then
+         estimate = estimate - 1
+        end
+        return estimate
+    end
+end
