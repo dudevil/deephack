8e5906c59e95a83efa14956808eea714476b981d
Taras Sereda <taras.y.sereda@gmail.com>
2015-07-21 02:33:06 +0300

    reward norm

--- dqn/NeuralQLearner.lua
+++ dqn/NeuralQLearner.lua
@@ -254,10 +254,11 @@ function nql:qLearnMinibatch()
 
     if self.reward_normalize then
         self.max_r = self.max_r*self.lambda + (1-self.lambda)*torch.max(r)
-        for i=1,#r:storage()
-            do
-            if r[i] > 0 then r[i] = r[i]/max_r end
+        for i=1,r:size()[1] do
+            if r[i] > 0 then 
+                r[i] = r[i]/self.max_r 
             end
+        end
     end
     local targets, delta, q2_max = self:getQUpdate{s=s, a=a, r=r, s2=s2,
         term=term, update_qmax=true}
